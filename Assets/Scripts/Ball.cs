ee21f201053705f4cd85682605faef335297c53e
rsato-username <satoheisuper@gmail.com>
2020-03-31 20:50:01 +0900

    create scripts of launcher and ball

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+	void Start()
+	{
+		this.GetComponent<Rigidbody>().velocity = new Vector3(-5, 0, 0);
+	}
+
+	void OnBecameInvisible()
+	{
+		Destroy(this.gameObject);
+	}
+}
---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+	[SerializeField] private float power;
+	public GameObject ballPrefab;
+	public GameObject timer;
+	public GameObject player;
+	public bool operation = true;
+	public bool timer_operation = true;
+	public bool player_operation = true;
+	public bool boost;
+
+	void Update()
+	{
+		timer_operation = timer.GetComponent<Timer>().operation;
+		player_operation = player.GetComponent<PlayerController>().operation;
+		if (timer_operation == false || player_operation == false)
+			operation = false;
+		if (this.operation)
+		{
+			power += Time.deltaTime;
+			if (boost)
+			{
+				power += Time.deltaTime;
+			} 
+			if (power >= 5.0f)
+			{
+				power = 0;
+				Instantiate(this.ballPrefab, this.transform);
+			}
+			if (timer.GetComponent<Timer>().timerText.text == "00:30")
+			{
+				boost = true;
+			}
+		}
+	}
+}
